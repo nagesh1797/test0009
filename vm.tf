6a5d050eeb55db409607b8672e061623d318edc4
kondal rao <raojonnala22@gmail.com>
2024-04-09 11:27:38 +0530

    added resource group

---
+++ vm.tf
@@ -0,0 +1,11 @@
+resource "azurerm_resource_group" "name77" {
+    name = "nagesh"
+    location = "eastus"
+  
+}
+
+resource "azurerm_resource_group" "name77" {
+    name = "nagesh7799"
+    location = "eastus"
+  
+}
